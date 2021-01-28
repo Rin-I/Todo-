0510ca25d1809f354fa27ff50f6890f4ed763f66
InamuraRin <75918034+InamuraRin@users.noreply.github.com>
2021-01-28 21:02:43 +0900

    html,jsを編集しました

--- tab.js
+++ tab.js
@@ -11,20 +11,20 @@
         Button.innerText = "done";
         TAG.appendChild(Button);
         $UL.appendChild(TAG);
+        
+        const $doneButton = document.querySelectorAll("button");
+        
+        const TaskDone = (e) =>{
+            e.preventDefault;
+            const $li = e.target.closest("li");
+            $li.classList.add("done");
+        }
+        
+        let TaskIndex = 0;
+        let TaskLen = document.querySelectorAll("button.OK").length;
+        while (TaskIndex < TaskLen) {
+            $doneButton[TaskIndex].addEventListener("click", (e)=> TaskDone(e));            
+            TaskIndex++;
+        }
     })
-
-    const $doneButton = document.querySelectorAll("button");
-
-    const TaskDone = (e) =>{
-        e.preventDefault;
-        const $li = e.target.closest("li");
-        $li.classList.add("done");
-    }
-
-    let TaskIndex = 0;
-    let TaskLen = document.querySelectorAll("button.OK").length;
-    while (TaskIndex < TaskLen) {
-        $doneButton[TaskIndex].addEventListener("click", (e)=> TaskDone(e));            
-        TaskIndex++;
-    }
-})();
+    })();
