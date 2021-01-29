e7a854eff6d6e1abb6c7888d2f3be82a03d13bb3
InamuraRin <75918034+InamuraRin@users.noreply.github.com>
2021-01-29 19:16:32 +0900

    サブタスク機能追加

--- tab.js
+++ tab.js
@@ -11,6 +11,13 @@
         Button.innerText = "done";
         TAG.appendChild(Button);
         $UL.appendChild(TAG);
+
+        //サブタスク箇所を追加
+        const SubTask = document.createElement("div");
+            SubTask.style.display = "none";
+            SubTask.classList.add("SubT");
+            SubTask.innerText = "サブタスク";
+            $UL.appendChild(SubTask);
         
         const $doneButton = document.querySelectorAll("button");
         
@@ -26,5 +33,27 @@
             $doneButton[TaskIndex].addEventListener("click", (e)=> TaskDone(e));            
             TaskIndex++;
         }
+        
+        let $SubT = document.getElementsByTagName("li");
+        let $SubTlen = $SubT.length;
+        let SubTIndex = 0;
+        while (SubTIndex < $SubTlen) {
+            $SubT[SubTIndex].addEventListener("click", ()=> {
+                if ($SubT[SubTIndex].nextElementSibling.style.display = "none") {
+                    $SubT[SubTIndex].nextElementSibling.style.display = "block";
+                }else{
+                $SubT[SubTIndex].nextElementSibling.style.display = "none";
+                }
+            });
+            SubTIndex++;
+
+        }
+        // $SubT[0].addEventListener("click", ()=> {
+        //     if ($SubT[0].nextElementSibling.style.display = "none") {
+        //         $SubT[0].nextElementSibling.style.display = "block";
+        //     }else{
+        //     $SubT[0].nextElementSibling.style.display = "none";
+        //     }
+        // })
     })
     })();
